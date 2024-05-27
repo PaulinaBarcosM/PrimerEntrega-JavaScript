d3ca012465c1f0758c196f551b24872cdb9f9146
Paulina Barcos Melchiori <paulinabarcosmelchiori@gmail.com>
2024-05-27 20:26:23 -0300

    programa terminado y agregado de ciclo

--- js/index.js
+++ js/index.js
@@ -36,33 +36,3 @@ if (edadMinima >= 18){
 } else {
     console.log("Lo sentimos, no cumples con la edad mínima requerida para viajar con TakeOff.");
 }
-
-// const EDAD_USUARIO = prompt("INGRESA TU EDAD");
-// console.log(EDAD_USUARIO);
-
-// const EDAD_MINIMA = parseInt(EDAD_USUARIO);
-
-//programa---------
-// if (isNaN(EDAD_MINIMA)){
-//     console.log ("Por favor, ingresa un numero");
-// }
-// else{
-//     if (EDAD_MINIMA >= 18){
-//         console.log("¡¡Eres mayor para viajar con takeOff!! :)");
-        
-//         let opciónDeViaje = prompt("Ingresa la opción de viaje que te interesa: Snow/Wake/Surf");
-//         alert("Opción no válida. Por favor, elige entre Snow, Wake o Surf.");
-//         console.log("A la brevedad te compartimos la información necesaria del viaje seleccionado");   
-        
-//     }
-//     else if(EDAD_MINIMA >= 15 && EDAD_MINIMA === 17){
-//         let permisoAutorizado = prompt("¿Tenes el permiso de tus padres para viajar?");
-//         if(permisoAutorizado === "si"){
-//             console.log("¡Tenemos una opción junior de viaje para ti!" + " " + "Comunicate con nosotros a través de takeOff@gmail.com");
-//         }else{
-//             console.log("¡Pedile permiso a tus papas y comunicate con nosotros!")
-//         }
-//     }else{
-//         console.log("Lo sentimos, no cumples con la edad mínima requerida para viajar con TakeOff.")
-//     }
-// }
